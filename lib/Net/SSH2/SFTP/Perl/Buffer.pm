92ebb586496e259421c2adf5babf33886ec815b6
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-03-21 16:04:07 +0100

    Initial commit

---
+++ lib/Net/SSH2/SFTP/Perl/Buffer.pm
@@ -0,0 +1,25 @@
+package Net::SSH2::SFTP::Perl::Buffer;
+
+use strict;
+use warnings;
+
+use parent qw[Net::SSH::Perl::Buffer];
+
+use Math::Int64 qw[net_to_int64 int64_to_net];
+
+sub new { shift->SUPER::new(@_) }
+
+sub get_int64 {
+    my $buf = shift;
+    my $off = defined $_[0] ? shift : $buf->{offset};
+    $buf->{offset} += 8;
+    net_to_int64($buf->bytes($off, 8));
+}
+
+sub put_int64 { shift->{buf} .= int64_to_net($_[0]) }
+
+sub get_attributes { Net::SSH2::SFTP::Perl::Attributes->new(Buffer => shift) }
+
+sub put_attributes { shift->{buf} .= $_[0]->as_buffer->bytes }
+
+1;
