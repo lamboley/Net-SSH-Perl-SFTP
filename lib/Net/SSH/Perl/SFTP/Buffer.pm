f9ec612919984da160ee9179f3a03833650c5f36
LAMBOLEY Lucas <lucaslamboley@outlook.com>
2019-04-23 16:43:28 +0200

    Update Buffer.pm

--- lib/Net/SSH/Perl/SFTP/Buffer.pm
+++ lib/Net/SSH/Perl/SFTP/Buffer.pm
@@ -1,75 +1,25 @@
-package Net::SSH::Perl::SFTP::Attributes;
+package Net::SSH::Perl::SFTP::Buffer;
 
 use strict;
 use warnings;
 
-use Net::SSH::Perl::SFTP::Buffer;
+use parent qw[Net::SSH::Perl::Buffer];
 
-our @FIELDS = qw[flags size uid gid perm atime mtime];
+use Math::Int64 qw[net_to_int64 int64_to_net];
 
-for my $f (@FIELDS) {
-    no strict 'refs';
-    *$f = sub { shift->{$f} };
+sub new { shift->SUPER::new(@_) }
+
+sub get_int64 {
+    my $buf = shift;
+    my $off = defined $_[0] ? shift : $buf->{offset};
+    $buf->{offset} += 8;
+    net_to_int64($buf->bytes($off, 8));
 }
 
-sub new {
-    my $class = shift;
-    my $a = bless { }, $class;
-    $a->init(@_);
-}
+sub put_int64 { shift->{buf} .= int64_to_net($_[0]) }
 
-sub init {
-    my ($a, %param) = @_;
-    $a->{$_} = 0 for (@FIELDS);
+sub get_attributes { Net::SSH::Perl::SFTP::Attributes->new(Buffer => shift) }
 
-    if (my $buf = $param{Buffer}) {
-        $a->{flags} = $buf->get_int32;
-
-        if ($a->{flags} & 0x01) {
-            $a->{size} = $buf->get_int64;
-        }
-        if ($a->{flags} & 0x02) {
-            $a->{uid} = $buf->get_int32;
-            $a->{gid} = $buf->get_int32;
-        }
-        if ($a->{flags} & 0x04) {
-            $a->{perm} = $buf->get_int32;
-        }
-        if ($a->{flags} & 0x08) {
-            $a->{atime} = $buf->get_int32;
-            $a->{mtime} = $buf->get_int32;
-        }
-        if ($a->{flags} & 0x80000000) {
-            my $extended_count = $buf->get_int32;
-            for (1..$extended_count) {
-                my $name = $buf->get_str;
-                my $value = $buf->get_str;
-                $a->{$name} = $value;
-            }
-        }
-    }
-    $a;
-}
-
-sub as_buffer {
-    my $a = shift;
-    my $buf = Net::SSH::Perl::SFTP::Buffer->new;
-    $buf->put_int32($a->{flags});
-    if ($a->{flags} & 0x01) {
-        $buf->put_int64(int $a->{size});
-    }
-    if ($a->{flags} & 0x02) {
-        $buf->put_int32($a->{uid});
-        $buf->put_int32($a->{gid});
-    }
-    if ($a->{flags} & 0x04) {
-        $buf->put_int32($a->{perm});
-    }
-    if ($a->{flags} & 0x08) {
-        $buf->put_int32($a->{atime});
-        $buf->put_int32($a->{mtime});
-    }
-    $buf;
-}
+sub put_attributes { shift->{buf} .= $_[0]->as_buffer->bytes }
 
 1;
